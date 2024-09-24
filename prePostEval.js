cb32b363b39445337f88aae52abc6a8011d3ca85
Snorre Rubin <31917416+Snorrski@users.noreply.github.com>
2024-09-24 12:31:49 +0200

    Update prePostEval.js

--- prePostEval.js
+++ prePostEval.js
@@ -1,5 +1,5 @@
 // Initialize global variables
-console.log("12:19")
+console.log("12:31")
 var lmsAPI = window.parent.parent;
 var p = GetPlayer();
 var iframe = window.parent.document.querySelector(`iframe[name="${window.name}"]`);
