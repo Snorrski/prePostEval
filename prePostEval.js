a014c5da96d26893d7c8dc2e2aa71081fd15fc05
Snorre Rubin <31917416+Snorrski@users.noreply.github.com>
2024-10-30 15:12:36 +0100

    Update prePostEval.js

--- prePostEval.js
+++ prePostEval.js
@@ -79,38 +79,38 @@ function hideLoading() {
 // Add CSS for spinner animation (you could add this to a <style> tag)
 const style = document.createElement('style');
 style.innerHTML = `
-	.likert {
- 		display: flex; 
-   		justify-content: space-between; 
-	 	align-items: center; 
-   		margin: 5px 0 20px 0;
-	 }
-  	.submitBtn {
-   		cursor: pointer; 
-	 	background: #14143c; 
-   		font-weight: bold;
-	 }
-  input.customRadio {
-  	display: none;
-  }
- 	input.customRadio:checked + label {background-color: #14143c; background-repeat: no-repeat; background-position: center; background-size: 10px 10px;border: 2px solid #14143c; background-image: url('data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 8 8%27%3e%3cpath fill=%27%23FFF%27 d=%27M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z%27/%3e%3c/svg%3e');";
-    input.customRadio + label {
-        display: inline-block; 
-        width: 20px; 
-        height: 20px; 
-        border-radius: 50%; 
-        background-color: #fff; 
-        border: 2px solid #14143c; 
-		cursor: pointer;
-        position: relative; /* Ensure z-index works */
-        z-index: 1; /* Keep the label in front of the line */
-    }
-	.radioWrapper {
-    	display: flex; 
-    	justify-content: center; 
-    	align-items: center; 
-    	height: 20px; /* Match the label height to avoid extra height */
-	}
+	// .likert {
+ 		// display: flex; 
+   		// justify-content: space-between; 
+	 	// align-items: center; 
+   		// margin: 5px 0 20px 0;
+	 // }
+  	// .submitBtn {
+   		// cursor: pointer; 
+	 	// background: #14143c; 
+   		// font-weight: bold;
+	 // }
+  // input.customRadio {
+  	// display: none;
+  // }
+ 	// input.customRadio:checked + label {background-color: #14143c; background-repeat: no-repeat; background-position: center; background-size: 10px 10px;border: 2px solid #14143c; background-image: url('data:image/svg+xml,%3csvg xmlns=%27http://www.w3.org/2000/svg%27 viewBox=%270 0 8 8%27%3e%3cpath fill=%27%23FFF%27 d=%27M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z%27/%3e%3c/svg%3e');";
+    // input.customRadio + label {
+        // display: inline-block; 
+        // width: 20px; 
+        // height: 20px; 
+        // border-radius: 50%; 
+        // background-color: #fff; 
+        // border: 2px solid #14143c; 
+		// cursor: pointer;
+        // position: relative; /* Ensure z-index works */
+        // z-index: 1; /* Keep the label in front of the line */
+    // }
+	// .radioWrapper {
+    	// display: flex; 
+    	// justify-content: center; 
+    	// align-items: center; 
+    	// height: 20px; /* Match the label height to avoid extra height */
+	// }
 	@keyframes spin {
         0% { transform: rotate(0deg); }
         100% { transform: rotate(360deg); }
@@ -207,7 +207,6 @@ function checkData(event = null, check = true) {
             const formData = new FormData(event.target);
             formData.forEach((value, key) => jsonData[key] = value);
         }
-
         sendDataToAPI(jsonData, check);
     } else {
         // If not all questions are answered, display a warning message
@@ -346,7 +345,7 @@ function addListenersToRadioButtons(form) {
         // Check if the focus is moving outside the form
         if (!form.contains(event.relatedTarget)) {
             console.log('Focus left the form, submitting...');
-		submitForm;
+			submitForm;
             //form.submit();  // Trigger form submission
         }
     });
